0cc46871a047e9c917b0f7c577d6e94919965a03
SarveshSimform <sarvesh.p@simformsolutions.com>
2022-04-06 17:58:25 +0530

    Practical 15 Uploaded.

---
+++ Practical-15/Practical-15/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Practical_15.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Index()
+        {
+            logger.LogInformation("Index method Called");
+            logger.LogWarning("Index method Called Warning");
+            return new string[] { "Hello World" };
+
+        }
+    }
+}
