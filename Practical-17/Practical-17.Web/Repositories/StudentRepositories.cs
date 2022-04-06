5c8d9696639abcdf1d35fa46f2b69c585db9cb43
SarveshSimform <sarvesh.p@simformsolutions.com>
2022-04-06 18:35:53 +0530

    Practical 17 Uploaded.

---
+++ Practical-17/Practical-17.Web/Repositories/StudentRepositories.cs
@@ -0,0 +1,16 @@
+﻿using Practical_17.Web.Contracts;
+using Practical_17.Web.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practical_17.Web.Repositories
+{
+    public class StudentRepositories : GenericRepositories<Student>, IStudentRepositories
+    {
+        public StudentRepositories(ApplicationDbContext context):base(context)
+        {
+
+        }
+    }
+}
---
+++ Practical-17/Practical-17/Migrations/20220406125705_kja.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Practical_17.Migrations
+{
+    public partial class kja : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
